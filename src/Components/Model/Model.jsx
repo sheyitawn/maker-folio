7313eb4a57631f3adb791e51049a622812d3f3b3
sheyitawn <randombuzzer234@gmail.com>
2024-12-01 16:22:38 +0000

    new model

--- src/Components/Model/Model.jsx
+++ src/Components/Model/Model.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
+import './model.css'
 
-const Model = ({ modelUrl, description }) => {
+const Model = ({ modelUrl }) => {
   const ModelLoader = ({ url }) => {
     const { scene } = useGLTF(url);
     return <primitive object={scene} />;
   };
 
   return (
-    <div style={{ width: '100%', height: '400px' }}>
-      <Canvas>
-        {/* Add lighting */}
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 10]} />
-        {/* Render 3D model */}
-        <ModelLoader url={modelUrl} />
-        {/* Add controls */}
-        <OrbitControls />
-      </Canvas>
-      <div style={{ textAlign: 'center', marginTop: '10px' }}>
-        <p>{description}</p>
+    <div className='model'>
+      <div className="model_model">
+        <Canvas>
+          {/* Add lighting */}
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[10, 10, 10]} />
+          {/* Render 3D model */}
+          <ModelLoader url={modelUrl} />
+          {/* Add controls */}
+          <OrbitControls />
+        </Canvas>
       </div>
+      
+
     </div>
   );
 };
