dec62d40059351f853066f2049bf9570bf6c6260
sheyitawn <randombuzzer234@gmail.com>
2025-04-05 17:35:07 +0100

    clean up here and there

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import DotCursor from "./Components/DotCursor/DotCursor";
+import Sketch from "./Components/Sketch/Sketch";
 import "./App.css";
 
-import Sketch from "./Components/Sketch/Sketch";
-import Project1 from "./Portfolio/Project1";
+import projects from "./Data/Projects.js";
 
 const App = () => {
-  // const models = [
-  //   { url: "/holo corn.glb", name: "deez" },
-  //   // { url: "/holo corn.glb", name: "deez" },
-  //   // { url: "/holo corn.glb", name: "deez" },
-
-  // ];
-
   return (
     <div className="content">
-
       <DotCursor />
       <div className="content_header">
         <div className="content_header-date">2020 - 2025</div>
@@ -27,35 +19,18 @@ const App = () => {
 
       <div className="content_sketch-container">
 
-        <Sketch
-          title="Gadget • Gesture Controlled Finger-tip"
-          description="This is a detailed description of Sketch 1."
-          model={"/test01.glb"}
-          customContent={<Project1 />}
-        />
-        <Sketch
-          title="Gadget • REVU, in-person reviews"
-          description="This is a detailed description of Sketch 2."
-          model={"/holo corn.glb"}
-          customContent={<img src="example.jpg" alt="Example" />}
-        />
-        <Sketch
-          title="Prop • Adventure Time Voice Box"
-          description="This is a detailed description of Sketch 2."
-          model={"/holo corn.glb"}
-          customContent={<img src="example.jpg" alt="Example" />}
-        />
-
+        {projects.map((sketch, index) => (
+          <Sketch
+            key={index}
+            title={sketch.title}
+            description={sketch.description}
+            model={sketch.model}
+            sketch={sketch.sketch}
+            customContent={sketch.customContent}
+          />
+        ))}
 
       </div>
-        {/* {models.map((model, index) => (
-          <Model
-            key={index}
-            modelUrl={model.url}
-            description={model.name}
-          />
-        ))} */}
-
       </div>
 
       <div className="content_footnote">
