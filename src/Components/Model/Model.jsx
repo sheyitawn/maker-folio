f7f870b934cfe5c3290dde36a93ab6971c952c49
sheyitawn <randombuzzer234@gmail.com>
2025-04-06 19:35:13 +0100

    random changes

--- src/Components/Model/Model.jsx
+++ src/Components/Model/Model.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
-import './model.css';
+import './model.css'
 
 const Model = ({ modelUrl }) => {
   const ModelLoader = ({ url }) => {
@@ -10,16 +10,20 @@ const Model = ({ modelUrl }) => {
   };
 
   return (
-    <div className="model">
-      <div className="model_canvas">
-        <Canvas camera={{ position: [0, 0, 3], fov: 45 }}>
-          <ambientLight intensity={0.5} />
-          <directionalLight intensity={1} position={[5, 5, 5]} />
-          <pointLight intensity={0.5} position={[-5, -5, -5]} />
+    <div className='model'>
+      <div className="model_model">
+        <Canvas>
+          {/* Add lighting */}
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[10, 10, 10]} />
+          {/* Render 3D model */}
           <ModelLoader url={modelUrl} />
-          <OrbitControls enableZoom={true} enablePan={false} dampingFactor={0.1} rotateSpeed={0.5} />
+          {/* Add controls */}
+          <OrbitControls />
         </Canvas>
       </div>
+      
+
     </div>
   );
 };
