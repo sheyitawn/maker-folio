41c9e2e8a67f9a87007b5348f4ed5867e468eea3
sheyitawn <randombuzzer234@gmail.com>
2025-04-06 22:54:47 +0100

    fixed cursor effects

--- src/Components/DotCursor/DotCursor.jsx
+++ src/Components/DotCursor/DotCursor.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import './dotcursor.css'
+import './dotcursor.css';
 
 const DotCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
       setPosition({ x: event.clientX, y: event.clientY });
+
+      // Check if we're hovering a .hover-target element
+      const target = document.elementFromPoint(event.clientX, event.clientY);
+      if (target?.closest('.hover-target')) {
+        setIsHovering(true);
+        console.log('hovering')
+      } else {
+        setIsHovering(false);
+        console.log('not hovering')
+
+      }
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -18,10 +30,10 @@ const DotCursor = () => {
 
   return (
     <div
-      className="dot-cursor"
+      className={`dot-cursor ${isHovering ? 'dot-cursor--active' : ''}`}
       style={{
-        top: `${position.y-23}px`,
-        left: `${position.x -23}px`,
+        top: `${position.y}px`,
+        left: `${position.x}px`,
       }}
     />
   );
