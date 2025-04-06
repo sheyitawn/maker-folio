1af703642ed31a13ec2ab4b293eb2d6dc7a75f49
sheyitawn <randombuzzer234@gmail.com>
2025-04-06 19:32:01 +0100

    moved main file

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import LoadingScreen from './Components/LoadingScreen/LoadingScreen';
+import Main from './Main'; // your actual app
+import "./Main.css";
 import DotCursor from "./Components/DotCursor/DotCursor";
-import Sketch from "./Components/Sketch/Sketch";
-import "./App.css";
-
-import projects from "./Data/Projects.js";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000); // 2.5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+      
   return (
-    <div className="content">
+    <>
       <DotCursor />
-      <div className="content_header">
-        <div className="content_header-date">2020 - 2025</div>
-        <div className="content_header-title">MAKER_PORTFOLIO</div>
-      </div>
-
-      <div className="content_box">
-      <div className="content_graph-paper"></div>
-
-      <div className="content_sketch-container">
-
-        {projects.map((sketch, index) => (
-          <Sketch
-            key={index}
-            title={sketch.title}
-            description={sketch.description}
-            model={sketch.model}
-            sketch={sketch.sketch}
-            customContent={sketch.customContent}
-          />
-        ))}
-
-      </div>
-      </div>
-
-      <div className="content_footnote">
-        <div className="content_footnote_header">built: 2020 – 2025</div>
-        <div className="content_footnote_center">// DEV NOTES</div>
-        <div className="content_footnote_footer">links</div>
-    
-      </div>
-
-    </div>
+      <div className="scanlines" />
+      {loading ? <LoadingScreen onFinish={() => setLoading(false)} /> : <Main />}
+    </>
   );
+  
 };
 
 export default App;
